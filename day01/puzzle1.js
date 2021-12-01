4ddf8f85536e0a6ff447030c4d5d7daefa8fb53f
mawrkus <web@sparring-partner.be>
2021-12-01 23:30:33 +0100

    Day 1: first puzzle

--- day01/puzzle1.js
+++ day01/puzzle1.js
@@ -2,4 +2,18 @@ const textInput = require('./input');
 
 const input = textInput.split("\n").filter(Boolean);
 
+let prevDepth = input[0];
+
+const output = input.reduce(
+  (acc, depth) => {
+    const diff = depth - prevDepth;
+
+    prevDepth = depth;
+
+    return (diff > 0) ? acc + 1 : acc;
+  },
+  0
+);
+
 console.log(input);
+console.log(output);
